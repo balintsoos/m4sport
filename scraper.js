f94189525c0a2fcb55fb47cf23d104dd0ea54875
Balint Soos <soosbalint95@gmail.com>
2025-10-08 00:57:54 +0200

    refactor: extract scraper logic into smaller functions for clarity

--- scraper.js
+++ scraper.js
@@ -3,42 +3,60 @@ import fs from "fs";
 import path from "path";
 
 const pageUrl = "https://m4sport.hu/elo";
-const manifestFileName = "index.m3u8";
-const playerFrameFileName = "player.php";
+const playerFrameSourceUrl = "player.php";
+const manifestUrlPattern = /"file":\s*"([^"?]+)/;
+const outputDirectoryPath = "./scraped";
+const outputFileName = "manifest-url.json";
 
 (async () => {
-  let manifestUrl = null;
   const browser = await firefox.launch({ headless: true });
   const page = await browser.newPage();
+  await page.goto(pageUrl, { waitUntil: "load" });
 
-  await page.goto(pageUrl, { waitUntil: "networkidle" });
-
-  for (const frame of page.frames()) {
-    if (frame.url().includes(playerFrameFileName)) {
-      const frameHtml = await frame.content();
-      if (frameHtml && frameHtml.includes(manifestFileName)) {
-        const match = frameHtml.match(/"file":\s*"([^"?]+)/);
-        manifestUrl = match ? match[1].replace(/\\\//g, "/") : null;
-      }
-    }
-  }
-
-  await browser.close();
-
-  if (!manifestUrl) {
-    console.error("No URL found");
+  const playerFrame = getPlayerFrame(page);
+  if (!playerFrame) {
+    console.error("Player frame not found");
+    await browser.close();
     process.exit(1);
   }
 
-  console.log("Found URL:", manifestUrl);
-  const updatedAt = new Date();
+  const manifestUrl = await getManifestUrl(playerFrame);
+  await browser.close();
+  if (!manifestUrl) {
+    console.error("Manifest URL not found in player frame");
+    process.exit(1);
+  }
 
-  const outputDir = path.resolve("./scraped");
-  fs.mkdirSync(outputDir, { recursive: true });
-  const outFile = path.join(outputDir, "manifest-url.json");
-  fs.writeFileSync(
-    outFile,
-    JSON.stringify({ manifestUrl, updatedAt }, null, 2),
-    { encoding: "utf-8" }
-  );
+  console.log("Manifest URL found:", manifestUrl);
+  const outputFileContent = getOutputFileContent(manifestUrl);
+  writeOutputFile(outputFileContent);
+  process.exit(0);
 })();
+
+function getPlayerFrame(page) {
+  return page
+    .frames()
+    .find((frame) => frame.url().includes(playerFrameSourceUrl));
+}
+
+async function getManifestUrl(playerFrame) {
+  const playerFrameContent = await playerFrame.content();
+  const matches = playerFrameContent.match(manifestUrlPattern);
+  return matches ? cleanUpEscapedSlashes(matches[1]) : null;
+}
+
+function cleanUpEscapedSlashes(url) {
+  return url.replace(/\\\//g, "/");
+}
+
+function writeOutputFile(fileContent) {
+  const outputDirectory = path.resolve(outputDirectoryPath);
+  fs.mkdirSync(outputDirectory, { recursive: true });
+  const outFile = path.join(outputDirectory, outputFileName);
+  fs.writeFileSync(outFile, fileContent, { encoding: "utf-8" });
+}
+
+function getOutputFileContent(manifestUrl) {
+  const updatedAt = new Date().toISOString();
+  return JSON.stringify({ manifestUrl, updatedAt }, null, 2);
+}
